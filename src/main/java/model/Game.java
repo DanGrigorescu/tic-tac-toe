c55e282a10dd94f4f222b2798d78e5f991106c88
DanGrigorescu <151735112+DanGrigorescu@users.noreply.github.com>
2023-11-22 12:49:54 +0200

    Crearea claselor necesare

---
+++ src/main/java/model/Game.java
@@ -0,0 +1,14 @@
+package model;
+
+import lombok.Data;
+
+@Data
+public class Game {
+
+    private String gameId;
+    Player player1;
+    Player player2;
+    private GameStatus status;
+    private int [][] board;
+    private TicToe winner;
+}
---
+++ src/main/java/storage/GameStorage.java
@@ -0,0 +1,32 @@
+package storage;
+import model.Game;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GameStorage {
+    private static Map<String, Game> games;
+    private static GameStorage instace;
+
+    private GameStorage(){
+        games= new HashMap<>();
+
+    }
+    public static synchronized GameStorage getInstace(){
+        if(instace==null){
+            instace = new GameStorage();
+        }
+        return instace;
+
+    }
+    public Map<String, Game> getGames(){
+        return games;
+    }
+
+    public void setGame(Game game) {
+        games.put(game.getGameId(), game);
+    }
+
+
+
+}
