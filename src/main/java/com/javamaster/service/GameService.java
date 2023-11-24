14c80fb6945ae4db2ce61dcbf89576ed700de01f
DanGrigorescu <151735112+DanGrigorescu@users.noreply.github.com>
2023-11-24 13:04:38 +0200

    check winner

--- src/main/java/com/javamaster/service/GameService.java
+++ src/main/java/com/javamaster/service/GameService.java
@@ -2,18 +2,15 @@ package com.javamaster.service;
 import com.javamaster.exception.InvalidGameException;
 import com.javamaster.exception.InvalidParamException;
 import com.javamaster.exception.NotFoundException;
-import com.javamaster.model.Game;
-import com.javamaster.model.GameStatus;
-import com.javamaster.model.Player;
+import com.javamaster.model.*;
 import com.javamaster.storage.GameStorage;
 
 import java.util.UUID;
 
-import static com.javamaster.model.GameStatus.IN_PROGRESS;
-import static com.javamaster.model.GameStatus.NEW;
+import static com.javamaster.model.GameStatus.*;
 
 public class GameService {
-    public Game createGame(Player player){
+    public Game createGame(Player player) {
         Game game = new Game();
         game.setBoard(new int[3][3]);
         game.setGameId(UUID.randomUUID().toString());
@@ -22,13 +19,14 @@ public class GameService {
         GameStorage.getInstace().setGame(game);
         return game;
     }
+
     public Game connectToGame(Player player2, String gameId) throws InvalidParamException, InvalidGameException {
-        if(!GameStorage.getInstace().getGames().containsKey(gameId)){
+        if (!GameStorage.getInstace().getGames().containsKey(gameId)) {
             throw new InvalidParamException("Nu exista joc cu acest id!");
         }
         Game game = GameStorage.getInstace().getGames().get(gameId);
 
-        if(game.getPlayer2() != null) {
+        if (game.getPlayer2() != null) {
             throw new InvalidGameException("Jocul nu mai este valabil!");
 
         }
@@ -36,14 +34,60 @@ public class GameService {
         game.setStatus(IN_PROGRESS);
         GameStorage.getInstace().setGame(game);
         return game;
-        }
+    }
+
     public Game connectToRandomGame(Player player2) throws NotFoundException {
-        Game game = GameStorage.getInstace().getGames().values().stream().filter(it->it.getStatus()
-                    .equals(NEW)).findFirst().orElseThrow(()->new NotFoundException("Jocul nu a fost gasit!"));
+        Game game = GameStorage.getInstace().getGames().values().stream().filter(it -> it.getStatus()
+                .equals(NEW)).findFirst().orElseThrow(() -> new NotFoundException("Jocul nu a fost gasit!"));
         game.setPlayer2(player2);
         game.setStatus(IN_PROGRESS);
         GameStorage.getInstace().setGame(game);
         return game;
     }
+
+    public Game gamePlay(GamePlay gamePlay) throws NotFoundException, InvalidGameException {
+        if (!GameStorage.getInstace().getGames().containsKey(gamePlay.getGameId())) {
+            throw new NotFoundException("Jocul nu a fost gasit!");
+        }
+        Game game = GameStorage.getInstace().getGames().get(gamePlay.getGameId());
+        if (game.getStatus().equals(FINISHED)) {
+            throw new InvalidGameException("Jocul este deja terminat!");
+        }
+        int[][] board = game.getBoard();
+        board[gamePlay.getCoordinateX()][gamePlay.getCoordinateY()] = gamePlay.getType().getValue();
+
+        checkWinner(game.getBoard(), TicToe.X);
+        checkWinner(game.getBoard(), TicToe.O);
+
+        GameStorage.getInstace().setGame(game);
+        return game;
     }
+
+    private Boolean checkWinner(int[][] board, TicToe ticToe) {
+        int[] boardArray = new int[9];
+        int counterIndex = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                boardArray[counterIndex] = board[i][j];
+                counterIndex++;
+            }
+        }
+        int[][] winCombinations = {{0, 1, 2}, {3, 4, 5,}, {6, 7, 8},
+                {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+                {0, 4, 8}, {2, 4, 6}};
+        for (int i = 0; i < winCombinations.length; i++) {
+            int counter = 0;
+            for (int j = 0; j < winCombinations.length; j++) {
+                if (boardArray[winCombinations[i][j]] == ticToe.getValue()) {
+                    counter++;
+                    if(counter == 3){
+                        return true;
+                    }
+
+                }
+            }
+        }
+        return false;
+    }
+}
 
